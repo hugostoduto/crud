9f95767c9ba3aff15277f4935de66cd02c7cd5e9
Hugo Stoduto <hugoarstoduto@gmail.com>
2022-05-21 21:10:10 -0300

    git@github.com:hugostoduto/crud.git

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div
+      className={`
+      flex h-screen
+      justify-center
+      items-center
+    `}
+    >
+      <span>Texto</span>
+    </div>
+  );
+}
