97bad2ef34d28e3cb18d87e96de4c2d9d07b43fd
Hugo Stoduto <hugoarstoduto@gmail.com>
2022-05-22 10:15:03 -0300

    Adicionando estilo ao compenente Table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -8,27 +8,39 @@ export default function Table(props: TableProps) {
   function Header() {
     return (
       <tr>
-        <th>Código </th>
-        <th>Nome</th>
-        <th>idade</th>
+        <th className="text-left p-4">Código </th>
+        <th className="text-left p-4">Nome</th>
+        <th className="text-left p-4">idade</th>
       </tr>
     );
   }
   function Data() {
     return props.clients?.map((client, i) => {
       return (
-        <tr key={client.id}>
-          <td>{client.id}</td>
-          <td>{client.name}</td>
-          <td>{client.age}</td>
+        <tr
+          key={client.id}
+          className={`
+          ${i % 2 === 0 ? "bg-blue-300" : "bg-blue-200"}`}
+        >
+          <td className="text-left p-4">{client.id}</td>
+          <td className="text-left p-4">{client.name}</td>
+          <td className="text-left p-4">{client.age}</td>
         </tr>
       );
     });
   }
 
   return (
-    <table>
-      <thead>{Header()}</thead>
+    <table className="w-full rounded-xl overflow-hidden ">
+      <thead
+        className={`
+        bg-gradient-to-r from-blue-500 to-blue-700
+        text-gray-100
+
+        `}
+      >
+        {Header()}
+      </thead>
       <tbody>{Data()}</tbody>
     </table>
   );
